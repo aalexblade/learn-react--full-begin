3566bfe5b177d5317e6af85d0ec58a3a069f0cf4
alex blade <aalexblade@gmail.com>
2025-02-10 21:14:45 +0200

    iper-declar

--- src/main.jsx
+++ src/main.jsx
@@ -31,88 +31,88 @@ import "./index.css";
 
 // ====================================================
 
-const root = createRoot(document.getElementById("root"));
+// const root = createRoot(document.getElementById("root"));
 
-function MyAwesomeNavbar() {
-  return (
-    <nav
-      className="navbar navbar-expand-sm navbar-dark bg-dark"
-      aria-label="Third navbar example"
-    >
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          MyAwesomeNavbar
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarsExample03"
-          aria-controls="navbarsExample03"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+// function MyAwesomeNavbar() {
+//   return (
+//     <nav
+//       className="navbar navbar-expand-sm navbar-dark bg-dark"
+//       aria-label="Third navbar example"
+//     >
+//       <div className="container-fluid">
+//         <a className="navbar-brand" href="#">
+//           MyAwesomeNavbar
+//         </a>
+//         <button
+//           className="navbar-toggler"
+//           type="button"
+//           data-bs-toggle="collapse"
+//           data-bs-target="#navbarsExample03"
+//           aria-controls="navbarsExample03"
+//           aria-expanded="false"
+//           aria-label="Toggle navigation"
+//         >
+//           <span className="navbar-toggler-icon"></span>
+//         </button>
 
-        <div className="collapse navbar-collapse" id="navbarsExample03">
-          <ul className="navbar-nav me-auto mb-2 mb-sm-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Link
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" aria-disabled="true">
-                Disabled
-              </a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-          <form role="search">
-            <input
-              className="form-control"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-          </form>
-        </div>
-      </div>
-    </nav>
-  );
-}
+//         <div className="collapse navbar-collapse" id="navbarsExample03">
+//           <ul className="navbar-nav me-auto mb-2 mb-sm-0">
+//             <li className="nav-item">
+//               <a className="nav-link active" aria-current="page" href="#">
+//                 Home
+//               </a>
+//             </li>
+//             <li className="nav-item">
+//               <a className="nav-link" href="#">
+//                 Link
+//               </a>
+//             </li>
+//             <li className="nav-item">
+//               <a className="nav-link disabled" aria-disabled="true">
+//                 Disabled
+//               </a>
+//             </li>
+//             <li className="nav-item dropdown">
+//               <a
+//                 className="nav-link dropdown-toggle"
+//                 href="#"
+//                 data-bs-toggle="dropdown"
+//                 aria-expanded="false"
+//               >
+//                 Dropdown
+//               </a>
+//               <ul className="dropdown-menu">
+//                 <li>
+//                   <a className="dropdown-item" href="#">
+//                     Action
+//                   </a>
+//                 </li>
+//                 <li>
+//                   <a className="dropdown-item" href="#">
+//                     Another action
+//                   </a>
+//                 </li>
+//                 <li>
+//                   <a className="dropdown-item" href="#">
+//                     Something else here
+//                   </a>
+//                 </li>
+//               </ul>
+//             </li>
+//           </ul>
+//           <form role="search">
+//             <input
+//               className="form-control"
+//               type="search"
+//               placeholder="Search"
+//               aria-label="Search"
+//             />
+//           </form>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// }
 
 /**
  * Challenge:
@@ -123,15 +123,29 @@ function MyAwesomeNavbar() {
  * Afterward, render it on the line below MyAwesomeNavbar
  */
 
-function MainContent() {
-  return <h1>react is greate </h1>;
-}
+// function MainContent(){
+//   return <h1>Hello react!</h1>
+// }
 
-root.render(
-  <div>
-    <MyAwesomeNavbar />
-    <MainContent />
-  </div>
+// root.render(
+//   <>
+//     <MyAwesomeNavbar />
+//   <MainContent/>
+//   </>
+// );
+// ===============================================================
+
+// Imperative code
+
+// const h1 = document.createElement("h1");
+// h1.textContent = "This is my first element from React!";
+// h1.className = "header";
+// document.getElementById("root").appendChild(h1);
+
+// ==================================================================
+
+// Declarative
+
+createRoot(document.getElementById("root")).render(
+  <h1 className="header">Hello my first element from React!</h1>
 );
-
-
