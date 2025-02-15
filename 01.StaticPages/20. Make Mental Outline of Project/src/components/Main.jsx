40a4f2b3e3c10d45acf62e0b95dc0adff0cbe28e
alex blade <aalexblade@gmail.com>
2025-02-15 21:40:50 +0200

    add  Navbar and Main

---
+++ 01.StaticPages/20. Make Mental Outline of Project/src/components/Main.jsx
@@ -0,0 +1,16 @@
+function Main() {
+  return (
+    <main>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first releaded in 2013</li>
+        <li>Was originally created by Jordon Walke</li>
+        <li>Has well over 100K stars on GtiHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Power thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
+}
+
+export default Main;
