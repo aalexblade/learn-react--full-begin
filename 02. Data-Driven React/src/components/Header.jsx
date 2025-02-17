96a92a177ae165d0156b4e5eb922bda219fc6ca6
alex blade <aalexblade@gmail.com>
2025-02-17 21:22:13 +0200

    data-driven

---
+++ 02. Data-Driven React/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <img src="../img/Geirangerfjord.png" alt="" />
+      <h1>my travel jurnal.</h1>
+    </header>
+  );
+}
+
+export default Header;
