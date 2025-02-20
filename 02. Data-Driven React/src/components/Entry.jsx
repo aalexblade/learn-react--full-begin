b0d25f644f72be9eda41f6df805c5953e0be7de6
alex blade <aalexblade@gmail.com>
2025-02-20 20:44:55 +0200

    add markup

--- 02. Data-Driven React/src/components/Entry.jsx
+++ 02. Data-Driven React/src/components/Entry.jsx
@@ -9,7 +9,9 @@ function Entry() {
         <div className="article-wrap">
           <img src="src/img/marker.png" alt="local" />
           <span>JAPAN</span>
-          <a href=" ">View on Google Maps</a>
+          <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">
+            View on Google Maps
+          </a>
         </div>
 
         <h2 className="article-title">Mount Fuji</h2>
