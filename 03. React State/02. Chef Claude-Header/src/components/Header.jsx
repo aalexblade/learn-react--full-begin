b8f9f0ae21ca26722d797822decc9426a78a6fc5
alex blade <aalexblade@gmail.com>
2025-02-28 21:59:59 +0200

    add markup

---
+++ 03. React State/02. Chef Claude-Header/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import chefCloudLogo from '../images/chef-claude-icon.png'
+
+function Header(){
+    return(
+<header>
+    <img src={chefCloudLogo} />
+    <h1>Chef Claude</h1>
+</header>
+
+    )
+}
+
+export default Header
---
+++ 03. React State/02. Chef Claude-Header/src/components/Main.jsx
@@ -0,0 +1,16 @@
+function Main() {
+  return (
+    <main>
+      <form className="add-ingridient-form">
+        <input
+         type="text"
+         placeholder="e.g. oregano"
+         aria-label="Add ingredient" 
+         />
+        <button> Add ingredient</button>
+      </form>
+    </main>
+  );
+}
+
+export default Main;
