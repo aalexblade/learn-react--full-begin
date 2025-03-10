9ae4d81bc0dce1263d716062b1a6939539cd7ba0
alex blade <aalexblade@gmail.com>
2025-03-10 21:17:48 +0200

    add ref static state from useState

--- 03. React State/02. Chef Claude-Header/src/components/Main.jsx
+++ 03. React State/02. Chef Claude-Header/src/components/Main.jsx
@@ -1,8 +1,9 @@
 // import { useState } from "react";
 
+import React from "react";
 
 function Main() {
-  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+  const [ingredients, setIngredients] = React.useState([]);
 
   const ingredientsListItems = ingredients.map((ingredient) => {
     return <li key={ingredient}>{ingredient}</li>;
@@ -10,14 +11,11 @@ function Main() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log("form submited");
     const formData = new FormData(event.currentTarget);
     const newIngridient = formData.get("ingredient");
-    ingredients.push(newIngridient)
+    setIngredients((prevIngr) => [...prevIngr, newIngridient]);
     console.log(ingredients);
   }
-
-
 
   return (
     <main>
@@ -37,3 +35,36 @@ function Main() {
 }
 
 export default Main;
+
+// function Main() {
+//   const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+
+//   const ingredientsListItems = ingredients.map((ingredient) => {
+//     return <li key={ingredient}>{ingredient}</li>;
+//   });
+
+//   function handleSubmit(event) {
+//     event.preventDefault();
+//     console.log("form submited");
+//     const formData = new FormData(event.currentTarget);
+//     const newIngridient = formData.get("ingredient");
+//     ingredients.push(newIngridient)
+//     console.log(ingredients);
+//   }
+
+//   return (
+//     <main>
+//       <form onSubmit={handleSubmit} className="add-ingridient-form">
+//         <input
+//           type="text"
+//           placeholder="e.g. oregano"
+//           aria-label="Add ingredient"
+//           name="ingredient"
+//         />
+//         <button> Add ingredient</button>
+//       </form>
+
+//       <ul>{ingredientsListItems}</ul>
+//     </main>
+//   );
+// }
