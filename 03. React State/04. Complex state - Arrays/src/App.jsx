f6d8d7319f5e79611c0a995318275e842819acdd
alex blade <aalexblade@gmail.com>
2025-03-13 20:39:53 +0200

    add fn prevContact

--- 03. React State/04. Complex state - Arrays/src/App.jsx
+++ 03. React State/04. Complex state - Arrays/src/App.jsx
@@ -1,73 +1,9 @@
-// import React from "react";
-// import avatar from "./images/user.png";
-// import starEmpty from "./images/star-empty.png";
-// import starFilled from "./images/star-filled.png";
-
-// function App() {
-//   const [contact, setContact] = React.useState({
-//     firstName: "John",
-//     lastName: "Doe",
-//     phone: "+1 (212) 555-1212",
-//     email: "itsmyrealname@example.com",
-//     isFavorite: false,
-//   });
-
-//   let starIcon = contact.isFavorite ? starFilled : starEmpty;
-
-//   function toggleFavorite() {
-//     console.log("Toggle Favorite");
-//     setContact((prevContact) => {
-//       return {
-//         ...prevContact,
-//         isFavorite: !prevContact.isFavorite,
-//       };
-//     });
-//   }
-
-//   return (
-//     <main>
-//       <article className="card">
-//         <img
-//           src={avatar}
-//           className="avatar"
-//           alt="User profile picture of John Doe"
-//         />
-//         <div className="info">
-//           <button
-//             onClick={toggleFavorite}
-//             aria-pressed={contact.isFavorite}
-//             aria-label={
-//               contact.isFavorite ? "Remove from favorites" : "Add to favorites"
-//             }
-//             className="favorite-button"
-//           >
-//             <img
-//               src={starIcon}
-//               alt={contact.isFavorite ? "filled star icon" : "empty star icon"}
-//               className="favorite"
-//             />
-//           </button>
-//           <h2 className="name">
-//             {contact.firstName} {contact.lastName}
-//           </h2>
-//           <p className="contact">{contact.phone}</p>
-//           <p className="contact">{contact.email}</p>
-//         </div>
-//       </article>
-//     </main>
-//   );
-// }
-
-// export default App;
-
-// ==========================================
-
 import React from "react";
 import avatar from "./images/user.png";
-import starFilled from "./images/star-filled.png";
 import starEmpty from "./images/star-empty.png";
+import starFilled from "./images/star-filled.png";
 
-export default function App() {
+function App() {
   const [contact, setContact] = React.useState({
     firstName: "John",
     lastName: "Doe",
@@ -80,10 +16,10 @@ export default function App() {
 
   function toggleFavorite() {
     console.log("Toggle Favorite");
-    setContact((prevIcon) => {
+    setContact((prevContact) => {
       return {
-        ...prevIcon,
-        isFavorite: !prevIcon.isFavorite,
+        ...prevContact,
+        isFavorite: !prevContact.isFavorite,
       };
     });
   }
@@ -99,10 +35,17 @@ export default function App() {
         <div className="info">
           <button
             onClick={toggleFavorite}
-            aria-pressed={false}
+            aria-pressed={contact.isFavorite}
+            aria-label={
+              contact.isFavorite ? "Remove from favorites" : "Add to favorites"
+            }
             className="favorite-button"
           >
-            <img src={starIcon} alt="empty star icon" className="favorite" />
+            <img
+              src={starIcon}
+              alt={contact.isFavorite ? "filled star icon" : "empty star icon"}
+              className="favorite"
+            />
           </button>
           <h2 className="name">
             {contact.firstName} {contact.lastName}
@@ -114,3 +57,63 @@ export default function App() {
     </main>
   );
 }
+
+export default App;
+
+// ==========================================
+
+
+// import React from "react"
+// import avatar from "./images/user.png"
+// import starFilled from "./images/star-filled.png"
+// import starEmpty from "./images/star-empty.png"
+
+// export default function App() {
+//     const [contact, setContact] = React.useState({
+//         firstName: "John",
+//         lastName: "Doe",
+//         phone: "+1 (212) 555-1212",
+//         email: "itsmyrealname@example.com",
+//         isFavorite: false
+//     })
+//     /**
+//      * Challenge: Fill in the values in the markup
+//      * using the properties of our state object above
+//      * (Ignore `isFavorite` for now)
+//      */
+
+//     function toggleFavorite() {
+//         console.log("Toggle Favorite")
+//     }
+
+//     return (
+//         <main>
+//             <article className="card">
+//                 <img
+//                     src={avatar}
+//                     className="avatar"
+//                     alt="User profile picture of John Doe"
+//                 />
+//                 <div className="info">
+//                     <button
+//                         onClick={toggleFavorite}
+//                         aria-pressed={false}
+//                         className="favorite-button"
+//                     >
+//                         <img
+//                             src={starEmpty}
+//                             alt="empty star icon"
+//                             className="favorite"
+//                         />
+//                     </button>
+//                     <h2 className="name">
+//                         John Doe
+//                     </h2>
+//                     <p className="contact">+1 (212) 555-1212</p>
+//                     <p className="contact">itsmyrealname@example.com</p>
+//                 </div>
+
+//             </article>
+//         </main>
+//     )
+// }
