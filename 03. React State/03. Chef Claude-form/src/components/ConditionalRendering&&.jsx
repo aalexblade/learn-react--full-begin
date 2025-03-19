e488ba32944046b82f9f8d79fa191e553ecbc994
alex blade <aalexblade@gmail.com>
2025-03-19 23:17:46 +0200

    add && lenght messages

--- 03. React State/03. Chef Claude-form/src/components/ConditionalRendering&&.jsx
+++ 03. React State/03. Chef Claude-form/src/components/ConditionalRendering&&.jsx
@@ -1,25 +1,50 @@
 import React from "react";
 
 function ConditionalRendering() {
-  const [unreadMessages, setUnreadMessages] = React.useState(['a','b']);
+  const [unreadMessages, setUnreadMessages] = React.useState(["a", "b"]);
 
   console.log(unreadMessages);
 
+  /**
+   * Challenge:
+   * - If there are no unread messages, display "You're all caught up!"
+   * - If there's exactly 1 unread message, it should read "You have
+   *   1 unread message" (singular)
+   * - If there are > 1 unread messages, display "You have <n> unread
+   *   messages" (plural)
+   */
+
   return (
     <>
-    {
-    unreadMessages.length > 0 && 
-      <h2>You have {unreadMessages.length} unread messages!</h2>
-    }
-  
-  </>
+      {unreadMessages.length > 0 && (
+        <h2>You have {unreadMessages.length} unread messages</h2>
+      )}
+      {unreadMessages.length === 0 && <h2> You're all caught up!</h2>}
+    </>
   );
 }
 
 export default ConditionalRendering;
 
-// 1==============================
+// 2===============================
 
+// function ConditionalRendering() {
+//   const [unreadMessages, setUnreadMessages] = React.useState(['a','b']);
+
+//   console.log(unreadMessages);
+
+//   return (
+//     <>
+//     {
+//     unreadMessages.length > 0 &&
+//       <h2>You have {unreadMessages.length} unread messages!</h2>
+//     }
+
+//   </>
+//   );
+// }
+
+// 1==============================
 
 // function ConditionalRendering() {
 //   const [unreadMessages, setUnreadMessages] = React.useState(['a','b']);
