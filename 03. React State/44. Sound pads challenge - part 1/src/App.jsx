01928331be68966de13e92cba5ae36a1cb919cc5
alex blade <aalexblade@gmail.com>
2025-03-31 22:27:38 +0300

    add dark mode

--- 03. React State/44. Sound pads challenge - part 1/src/App.jsx
+++ 03. React State/44. Sound pads challenge - part 1/src/App.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import padsData from "./pads";
 import Pad from "./Pad";
 
-export default function App() {
+export default function App({darkMode}) {
   const [pads, setPads] = React.useState(padsData);
 
+ const styles={
+  backgroundColor: darkMode ? "#c5bebe" : "#21eb3f"
+ }
+ 
+//     const styles = {
+  //     backgroundColor: darkMode ? "#222222" : "#cccccc",
+  //     color: pads.on ? "on" : "off",
+  //   };
+ 
   const buttonEl = pads.map((pad) => (
-  <Pad key={pad.id} color={pad.color} on={pad.on}/>
-));
+    <button key={pad.id} style={styles} ></button>
+  ));
 
   return (
     <main>
@@ -15,6 +24,22 @@ export default function App() {
     </main>
   );
 }
+
+// =====================================3
+
+// export default function App() {
+//   const [pads, setPads] = React.useState(padsData);
+
+//   const buttonEl = pads.map((pad) => (
+//   <Pad key={pad.id} color={pad.color} on={pad.on}/>
+// ));
+
+//   return (
+//     <main>
+//       <div className="pad-container">{}</div>
+//     </main>
+//   );
+// }
 
 // =====================================1
 
